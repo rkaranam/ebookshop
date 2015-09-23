09d5c0ba12b946a795b3b4825598b5482a21136e
Rajasekhar Karanam <karanam.rajasekhar44@gmail.com>
2015-09-23 11:13:11 +0530

    Need to work on driver not found issue

---
+++ WEB-INF/classes/QueryServlet.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.sql.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class QueryServlet extends HttpServlet{
+
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		// set the MIME type for the response message
+		response.setContentType("text/html");
+
+		// Get the output writer to write the response message into the network socket
+		PrintWriter out = response.getWriter();
+
+		Connection connection = null;
+		Statement statement = null;
+
+		try {
+
+			connection = DriverManager.getConnection(
+				"jdbc:mysql://localhost:3306/ebookshop", "root", "mysql");
+
+			System.out.println("MySQL Connection Successful!");
+
+		} catch(SQLException ex) {
+			ex.printStackTrace();
+		} finally {
+			out.close();
+			try {
+				if(statement != null) statement.close();
+				if(connection != null) connection.close();
+			} catch(Exception ex) {
+				ex.printStackTrace(); 
+			}
+		}
+
+	}
+}
