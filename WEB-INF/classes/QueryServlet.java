30d3cbcf563be0379e7d1a21c4f17e0037775346
Rajasekhar Karanam <karanam.rajasekhar44@gmail.com>
2015-09-24 23:20:19 +0530

    database.properties file is working now

--- WEB-INF/classes/QueryServlet.java
+++ WEB-INF/classes/QueryServlet.java
@@ -3,6 +3,8 @@ import java.sql.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 
+import java.util.Properties;
+
 public class QueryServlet extends HttpServlet{
 
 	@Override
@@ -15,22 +17,36 @@ public class QueryServlet extends HttpServlet{
 		// Get the output writer to write the response message into the network socket
 		PrintWriter out = response.getWriter();
 
+		ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+		Properties prop = new Properties();
+
 		Connection connection = null;
 		Statement statement = null;
 
 		try {
-
-			// loads the mysql Driver class into memory
+			
+			prop.load(classLoader.getResourceAsStream("database.properties"));
+			
 			Class.forName("com.mysql.jdbc.Driver");
 
 			connection = DriverManager.getConnection(
-				"jdbc:mysql://localhost:3306/ebookshop", "root", "mysql");
+				"jdbc:mysql://localhost:3306/ebookshop", 
+				prop.getProperty("username"), 
+				prop.getProperty("password"));
 
 			System.out.println("MySQL Connection Successful!");
 
 		} catch(SQLException ex) {
+			System.out.println("********** SQL Exception Occured ***********");
 			ex.printStackTrace();
 		} catch(ClassNotFoundException ex) {
+			System.out.println("********** Class Not Found Exception Occured ***********");
+			ex.printStackTrace();
+		} catch(IOException ex) {
+			System.out.println("********** IO Exception Occured ***********");
+			ex.printStackTrace();
+		} catch(Exception ex) {
+			System.out.println("********** Exception Occured ***********");
 			ex.printStackTrace();
 		} finally {
 			out.close();
