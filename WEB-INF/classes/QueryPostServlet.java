e8d852961ebad790fcddf5c765105e4894484fc5
Rajasekhar Karanam <karanam.rajasekhar44@gmail.com>
2015-09-26 19:24:37 +0530

    Working with doPost() method: Parameters are sent through request body not url

---
+++ WEB-INF/classes/QueryPostServlet.java
@@ -0,0 +1,99 @@
+import java.io.*;
+import java.sql.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+import java.util.Properties;
+import javax.servlet.annotation.*;
+
+@WebServlet(value = "/querypost")
+public class QueryPostServlet extends HttpServlet{
+
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		// set the MIME type for the response message
+		response.setContentType("text/html");
+
+		// Get the output writer to write the response message into the network socket
+		PrintWriter out = response.getWriter();
+
+		ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+		Properties prop = new Properties();
+
+		Connection connection = null;
+		Statement statement = null;
+
+		try {
+			
+			prop.load(classLoader.getResourceAsStream("database.properties"));
+			
+			Class.forName("com.mysql.jdbc.Driver");
+
+			connection = DriverManager.getConnection(
+				"jdbc:mysql://localhost:3306/ebookshop", 
+				prop.getProperty("username"), 
+				prop.getProperty("password"));
+
+			System.out.println("MySQL Connection Successful!");
+
+			statement = connection.createStatement();
+
+			String[] authors = request.getParameterValues("author");
+
+			if(null == authors){
+				out.println("<h2>Go back and choose author.</h2>");
+				return;
+			}
+
+			String queryString = "select * from books where author IN('" + authors[0];
+
+			for(int i = 1; i < authors.length; i++){
+				queryString += "', '" + authors[i];
+			}
+
+			queryString += "') and price < " + request.getParameter("price");
+
+			System.out.println(queryString);
+
+			out.println("<html><head><title>Query Results</title></head><body>");
+			out.println("<h2>Thank you for your query.</h2>");
+			out.println("<p>Your query is <b>" + queryString + "</b></p>");
+
+			ResultSet rs = statement.executeQuery(queryString);
+
+			int count = 0;
+			while(rs.next()){
+				out.println("<p>Title: " + rs.getString("title") +
+							", Author: " + rs.getString("author") +
+							", Price: $" + rs.getDouble("price") + "</p>");
+				count++;
+			}
+
+			out.println("<p>" + count + " records found.</p>");
+			out.println("</body></html>");
+
+		} catch(Exception ex) {
+			System.out.println("********** Exception Occured ************");
+			ex.printStackTrace();
+		} finally {
+			out.close();
+			try {
+				if(statement != null) statement.close();
+				if(connection != null) connection.close();
+			} catch(Exception ex) {
+				ex.printStackTrace(); 
+			}
+		}
+
+	}
+
+	@Override
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		// just redirecting to doGet() method
+		doGet(request, response);
+	}
+}
